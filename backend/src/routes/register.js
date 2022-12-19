c6120317af30477d5d3b6def2f9b8476d7939f27
Mychel Albuquerque <mychelalbu@gmail.com>
2022-12-19 07:32:23 -0300

    :sparkles: feat: creates register route and birthValidation and emailvalidation middlewares

--- backend/src/routes/register.js
+++ backend/src/routes/register.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const birthValidation = require('../middlewares/birthValidation');
+const emailValidation = require('../middlewares/emailValidation');
 const nameValidation = require('../middlewares/nameValidation');
 const router = express.Router();
 
 
-router.post('/', nameValidation, birthValidation, (req, res) => {
+router.post('/', nameValidation, emailValidation, birthValidation, (req, res) => {
   return res.status(200).json(req.body);
 });
 
